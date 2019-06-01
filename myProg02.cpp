7c6fea933833ab62bc57331d023f472fb7fdd64e
judyli840 <51252982+judyli840@users.noreply.github.com>
2019-05-31 20:51:42 -0700

    Add files via upload

---
+++ myProg02.cpp
@@ -0,0 +1,55 @@
+// countDucks.cpp 
+// P. Conrad for CS16, Winter 2015
+// Example program to read from file and count occurences
+
+#include <iostream> // for printf()
+#include <cstdlib> // for exit(), perror()
+#include <fstream> // for ifstream
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  if (argc!=2) {
+    // if argc is not 2, print an error message and exit
+    cerr << "Usage: "<< argv[0] << " inputFile" << endl;
+    exit(1); // defined in cstdlib
+  }
+  
+  ifstream ifs;
+  
+  ifs.open(argv[1]);
+
+  if( !ifs){
+	  cerr << "Open failed" << endl;
+	  exit(1);
+  }
+
+  int num = 0;
+  int all = 0;
+  int notDucks = 0;
+  string line= "";
+  while (!ifs.eof()){
+	  
+	  getline (ifs, line);
+
+	  
+	  int pos = line.find("duck");
+
+	  if (!(line == "")){
+		  all++;}
+
+	  if (pos >= 0){
+		  num++;
+	  }
+	  
+  }
+  notDucks = all - num;
+	  cout << "Report for " << argv[1] << ":" << endl;
+	  cout << "Animal count:    " << all << endl;
+	  cout << "Duck count:      " << num << endl;
+	  cout << "Non duck count:  " << notDucks << endl;
+
+  ifs.close();
+  return 0;
+}
